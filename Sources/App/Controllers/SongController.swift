cbbee065e9336699b6b47f145f4c38cfd433f643
Vitor Otero <vitorotero@icloud.com>
2022-04-25 01:12:14 +0100

    Create func

--- Sources/App/Controllers/SongController.swift
+++ Sources/App/Controllers/SongController.swift
@@ -14,10 +14,16 @@ struct SongController: RouteCollection {
         //routes means localhost/songs
         let songs = routes.grouped("songs")
         songs.get(use: index)
+        songs.post(use: create)
     }
     
     func index(req: Request) throws -> EventLoopFuture<[Song]> {
         //Get data from db >> Song Model
         return Song.query(on: req.db).all()
+    }
+    
+    func create(req: Request) throws -> EventLoopFuture<HTTPStatus> {
+        let song = try req.content.decode(Song.self)
+        return song.save(on: req.db).transform(to: .ok)
     }
 }
